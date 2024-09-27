7b8c217b43cb77454a1667f7e71eda5344d28121
nomankarim8 <karimnoman97@gmail.com>
2024-09-27 09:36:30 -0400

    commit

---
+++ nano binance_trading_bot.py
@@ -0,0 +1,10 @@
+# At First we will install Bash Command in our Terminal 
+
+# pip install binance-futures
+# pip install ta-lib
+# pip install pandas
+
+# now Start the code with enjoy the binance trading 
+# so Let's Start 
+
+
